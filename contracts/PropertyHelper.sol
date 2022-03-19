6934c2903a912e65ab52d5130f7621ebd6ec22bf
tauraspaj <pajuodis.tauras@gmail.com>
2022-03-19 14:13:27 +0000

    Property helper contract

---
+++ contracts/PropertyHelper.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./PropertyFactory.sol";
+
+contract PropertyHelper is PropertyFactory {
+    modifier onlyOwnerOf(uint _propertyId) {
+        require(_msgSender() == propertyToOwner[_propertyId]);
+        _;
+    }
+
+    function changePropertyName(uint _propertyId, string calldata _newName) external onlyOwnerOf(_propertyId) {
+        properties[_propertyId].name = _newName;
+    }
+}
