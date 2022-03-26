327ed629f8a79a98176aab59a5bb9e32df7285cd
tauraspaj <pajuodis.tauras@gmail.com>
2022-03-26 14:11:15 +0000

    Deploy ERC20TokenFactory

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const PropertyFactory = artifacts.require("PropertyFactory");
+const ERC20TokenFactory = artifacts.require("ERC20TokenFactory");
 
 module.exports = function(deployer) {
   deployer.deploy(PropertyFactory);
+  deployer.deploy(ERC20TokenFactory);
 };
