cb11c321f211571aa8e0fbfd8a34f14c2715a0d6
tauraspaj <pajuodis.tauras@gmail.com>
2022-04-07 12:15:07 +0100

    Include living area in property

--- contracts/PropertyFactory.sol
+++ contracts/PropertyFactory.sol
@@ -18,7 +18,8 @@ contract PropertyFactory is Context, Ownable {
         string postcode;
         uint16 nBedrooms;
         uint16 nShowers;
-        string[] images;
+        uint16 livingArea;
+        string[4] images;
         uint price;
         uint monthlyRent;
         address tokenAddress;
@@ -34,14 +35,15 @@ contract PropertyFactory is Context, Ownable {
     );
 
     constructor() {
+        createProperty("32 Madeup Road, UK", "MD2 RD8", 51, 4, 3200, ["https://i.gyazo.com/74ae883a4cae6657d9d52c26a315d46d.png", "https://i.gyazo.com/8fbb7e2bc4531633db182e9a8fe5a635.png", "https://i.gyazo.com/74ae883a4cae6657d9d52c26a315d46d.png", "https://i.gyazo.com/74ae883a4cae6657d9d52c26a315d46d.png"], 162000, 2500, 10000, "TKN");
     }
 
-    function _createProperty(string memory _propertyAddress, string memory _postcode, uint16 _nBedrooms, uint16 _nShowers, string[] memory _images, uint _price, uint _monthlyRent, uint _nTokens, string memory _tokenSymbol) internal {
+    function _createProperty(string memory _propertyAddress, string memory _postcode, uint16 _nBedrooms, uint16 _nShowers, uint16 _livingArea, string[4] memory _images, uint _price, uint _monthlyRent, uint _nTokens, string memory _tokenSymbol, address owner) internal {
         // Generate ERC20 token on this property
-        address tokenAddress = tokenFactory.deployNewERC20Token(_propertyAddress, _tokenSymbol, _nTokens);
+        address tokenAddress = tokenFactory.deployNewERC20Token(_propertyAddress, _tokenSymbol, _nTokens, owner);
 
         // Push property into the array
-        properties.push( Property(_propertyAddress, _postcode, _nBedrooms, _nShowers, _images, _price, _monthlyRent, tokenAddress) );
+        properties.push( Property(_propertyAddress, _postcode, _nBedrooms, _nShowers, _livingArea, _images, _price, _monthlyRent, tokenAddress) );
         uint id = properties.length - 1;
 
         // Assign property ownerhip
@@ -52,7 +54,11 @@ contract PropertyFactory is Context, Ownable {
         propertyCount++;
     }
 
-    function createProperty(string memory _propertyAddress, string memory _postcode, uint16 _nBedrooms, uint16 _nShowers, string[] memory _images, uint _price, uint _monthlyRent, uint _nTokens, string memory _tokenSymbol) public {
-        _createProperty(_propertyAddress, _postcode, _nBedrooms, _nShowers, _images, _price, _monthlyRent, _nTokens, _tokenSymbol);
+    function getImages(uint id) external view returns (string[4] memory image) {
+        return properties[id].images;
+    }
+
+    function createProperty(string memory _propertyAddress, string memory _postcode, uint16 _nBedrooms, uint16 _nShowers, uint16 _livingArea, string[4] memory _images, uint _price, uint _monthlyRent, uint _nTokens, string memory _tokenSymbol) public {
+        _createProperty(_propertyAddress, _postcode, _nBedrooms, _nShowers, _livingArea, _images, _price, _monthlyRent, _nTokens, _tokenSymbol, _msgSender());
     }
 }
