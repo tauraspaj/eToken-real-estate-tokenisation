41a4b6ad8c737c96f1dd397c315714034ad3230e
tauraspaj <pajuodis.tauras@gmail.com>
2022-04-22 00:12:40 +0100

    Delete old conversions and clean up code

--- public/js/app.js
+++ public/js/app.js
@@ -424,14 +424,6 @@ $(() => {
             const propertyId = url.split("=").pop();
             App.renderProperty(propertyId);
         }
-
-        // const amount = 0.0001
-        // web3.utils.toWei(amount.toString())
-        // console.log(amount);
-        // let tst = await toBaseUnit("152.62", 18, web3.utils.BN).toString()
-        // let ha = web3.utils.fromWei('152620000000000000000','ether')
-        // console.log(tst);
-        // console.log(ha);
     })
     // Mobile menu
     $('#burger-menu, #mobile-curtain, #close-mobile-overlay').on('click', () => $('#mobile-overlay').slideToggle())
@@ -485,56 +477,6 @@ $(() => {
     })
 
 })
-
-let isString = (s) => {
-    return (typeof s === 'string' || s instanceof String)
-}
-
-let toBaseUnit = (value, decimals, BN) => {
-    if (!isString(value)) {
-      throw new Error('Pass strings to prevent floating point precision issues.')
-    }
-    const ten = new BN(10);
-    const base = ten.pow(new BN(decimals));
-  
-    // Is it negative?
-    let negative = (value.substring(0, 1) === '-');
-    if (negative) {
-      value = value.substring(1);
-    }
-  
-    if (value === '.') { 
-      throw new Error(
-      `Invalid value ${value} cannot be converted to`
-      + ` base unit with ${decimals} decimals.`); 
-    }
-  
-    // Split it into a whole and fractional part
-    let comps = value.split('.');
-    if (comps.length > 2) { throw new Error('Too many decimal points'); }
-  
-    let whole = comps[0], fraction = comps[1];
-  
-    if (!whole) { whole = '0'; }
-    if (!fraction) { fraction = '0'; }
-    if (fraction.length > decimals) { 
-      throw new Error('Too many decimal places'); 
-    }
-  
-    while (fraction.length < decimals) {
-      fraction += '0';
-    }
-  
-    whole = new BN(whole);
-    fraction = new BN(fraction);
-    let wei = (whole.mul(base)).add(fraction);
-  
-    if (negative) {
-      wei = wei.neg();
-    }
-  
-    return new BN(wei.toString(10), 10);
-}
 
 let calculateRate = (totalSupply, propertyValue) => {
     return totalSupply/propertyValue;
